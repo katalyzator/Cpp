8410bc0ce70d65a7132b31d16974b40dc8bfd116
Katalyzator <web.coder96@gmail.com>
2016-03-19 17:23:44 +0600

    QuickSort and add gitignore file

---
+++ Quick_Sort/QuickSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int a[100];
+void quickSort(int l, int r)
+{
+    int x = a[l + (r - l) / 2];
+    //запись эквивалентна (l+r)/2,
+    //но не вызввает переполнения на больших данных
+    int i = l;
+    int j = r;
+    //код в while обычно выносят в процедуру particle
+    while(i <= j)
+    {
+        while(a[i] < x) i++;
+        while(a[j] > x) j--;
+        if(i <= j)
+        {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    }
+    if (i<r)
+                quickSort(i, r);
+   
+    if (l<j)   
+        quickSort(l, j);    
+}
+int main()
+{
+    int n;//количество элементов в массиве
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    quickSort(0, n-1);
+    for(int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }        
+    return 0;
+}
+
+    Описание
